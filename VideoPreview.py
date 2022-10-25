1ce97da5bbe301f470e648b40fe2b57b5b3466d8
angel <angel.shum@2t1l.co>
2022-10-25 10:56:53 +0800

    infinity number colors

--- VideoPreview.py
+++ VideoPreview.py
@@ -33,26 +33,33 @@ class VideoPreview():
         self.color_height = 100;
         self.number_colors = 6;
     
-    def show_image(self):
+    def showFrame(self):
         if self.frame is not None:  
             
             # need to use copy because this way wont edit the original frame that is saved in a memory address
             frame = copy.copy(self.frame);
-
+            
+            color = -1;
+            
+            # create colors bars
             for i in range(self.number_colors):
-                
                 size = (self.color_width, self.color_height);
                 cx, cy = self.x, int(self.video_height/2) - self.y;
                        
                 top_left = (cx, cy + (i*size[1]));
                 bottom_right = (cx + size[0], cy + (size[1] * (i+1)));
+
+                if (color < len(self.COLORS) - 1):
+                    color += 1;
+                else:
+                    color = 0;
                 
-                cv2.rectangle(frame, top_left, bottom_right, self.COLORS[i], -1);
-                
+                cv2.rectangle(frame, top_left, bottom_right, self.COLORS[color], -1);
+       
             cv2.imshow("Video", frame);
       
         if not self.keep:
-            self.root.after(1000, self.show_image)
+            self.root.after(1000, self.showFrame)
 
     def setValues(self, x, y, color_width, color_height, number_colors):
         self.x = x;
@@ -64,7 +71,7 @@ class VideoPreview():
     def start(self):    
         self.keep = False;
         self.thread.start();
-        self.root.after(1000, self.show_image);
+        self.root.after(1000, self.showFrame);
 
     def stop(self):
         self.keep = True;
